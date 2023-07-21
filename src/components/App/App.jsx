616b2fbcd12569751ee40400bb292379ae940588
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-07-21 17:05:03 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,36 +7,37 @@ import { ImageGallery } from "components/ImageGallery/ImageGallery";
 import { Modal } from "components/Modal/Modal";
 import { Button } from "components/Button/Button";
 
+const API_KEY = '38387021-e8462f34030ce37ed84fa82f8';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+
+async function fetchArticles(form, currentPage) {
+
+  
+  try {
+    const res = await axios.get(`?q=cat&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
+    // console.log(res.data.hits);
+    const photos = res.data.hits;
+    
+    console.log(photos);
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export class App extends Component {
 
-  const API_KEY = '38387021-e8462f34030ce37ed84fa82f8';
-  axios.defaults.baseURL = 'https://pixabay.com/api/';
-  
-  
-  async function fetchArticles(form, currentPage) {
-
-    
-    try {
-      const res = await axios.get(`?q=cat&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
-      // console.log(res.data.hits);
-      const photos = res.data.hits;
-      
-      console.log(photos);
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-
   render () {
+    return (
       <AppStyled>
         <Searchbar />
         <SearchForm />
         <ImageGallery />
         <Modal />
         <Button />
-    </AppStyled>
+      </AppStyled>
+    )
+
   }
 
 };
